5c3eda329d634af8d44c377410a525abbde3d461
Aleximiy <ktyf121205@gmail.com>
2026-06-26 15:10:56 +0300

    login logic backend works correctly

--- data/init_db_DO_NOT_TOUCH.py
+++ data/init_db_DO_NOT_TOUCH.py
@@ -4,10 +4,14 @@ def init_db():
     connection = sqlite3.connect('logins.db')
     cursor = connection.cursor()
 
+    #cursor.execute('DROP TABLE IF EXISTS logins;')
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS logins
-        (login VARCHAR(16), password VARBINARY(20), is_admin BOOL)
+        (login VARCHAR(16), password VARBINARY(256), is_admin BOOL);
                 ''')
 
     connection.commit()
     connection.close()
+
+#init_db()
